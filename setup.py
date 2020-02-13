57f0c192a4fb4cd664555145156cb842bbc95791
maxfrenzel <maxfrenzel@gmail.com>
2020-02-13 16:50:37 +0900

    Updated setup and readme for PyPI distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cvae",
-    version="0.0.1",
+    version="0.0.2",
     author="Max Frenzel",
     author_email="maxfrenzel+cvae@gmail.com",
     description="CompressionVAE: General purpose dimensionality reduction and manifold learning tool based on "
